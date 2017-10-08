3ec40154e31790267644789f79dfb7e21d8708e0
Annette Michaels <micha232@umn.edu>
2017-10-08 23:40:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,19 +10,77 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random()*100)
+  var addItem = { [item]: price }
+  cart.push(addItem)
+  console.log(`${Object.keys(addItem)} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  //var retString = ["In your cart, you have "]
+  var retString = []
+  var cartlength = cart.length
+
+  if (cartlength == 0) {
+    console.log("Your shopping cart is empty.")
+
+  } else if (cartlength == 1) {
+    var key = Object.keys(cart[0])[0]
+    console.log(`In your cart, you have ${key} at $${cart[0][key]}.`)
+
+  } else if (cartlength == 2) {
+    console.log(cartlength + " 2")
+    var key1 = Object.keys(cart[0])[0]
+    var key2 = Object.keys(cart[1])[0]
+    console.log(`In your cart, you have ${key1} at $${cart[0][key1]} and ${key2} at $${cart[1][key2]}.`)
+
+  } else {
+    for (var i = 0; i <= cartlength-1; i++) {
+      var key = Object.keys(cart[i])[0]
+//      console.log(`i equals ${i} and length equals ${cartlength} key is ${key}`)
+//      console.log(`i equals ${i} and length -1 equals ${cartlength-1}`)
+      if (i == cartlength-1) {
+//        console.log("last iteration if statement")
+        console.log(`In your cart, you have` + `${retString.toString()}, and ${key} at $${cart[i][key]}.`)
+
+        //we want In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.
+
+      } else {
+        console.log("push to array")
+        retString.push(` ${key} at $${cart[i][key]}`)
+      }
+    }
+  }
 }
 
 function total() {
-  // write your code here
+  var totalMoney = 0
+  var cartLength = cart.length
+  for (var i = 0; i < cartLength; i++) {
+    totalMoney = totalMoney + cart[i][Object.keys(cart[i])[0]]
+  }
+  return totalMoney
 }
 
 function removeFromCart(item) {
-  // write your code here
+  if (Boolean(item.hasOwnProperty)) {
+    console.log("if loop")
+    var cartLength = cart.length
+    console.log(cartLength)
+    for (var i = 0; i < cartLength; i++) {
+
+      //if statement broken
+      if (Object.keys(cart[i]) == item) {
+        delete cart[i].item
+        return cart
+      }
+      console.log("I shouldn't get here")
+    }
+  } else {
+    console.log("That item is not in your cart.")
+    return cart
+  }
 }
 
 function placeOrder(cardNumber) {
