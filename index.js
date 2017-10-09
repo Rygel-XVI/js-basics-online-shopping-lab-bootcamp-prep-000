d8966d2089b050c7a25c3892d18c2ce9b9427578
Annette Michaels <micha232@umn.edu>
2017-10-09 00:07:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,14 +64,13 @@ function total() {
 }
 
 function removeFromCart(item) {
-  if (Boolean(item.hasOwnProperty)) {
+  if (item.hasOwnProperty) {
     console.log("if loop")
     var cartLength = cart.length
     console.log(cartLength)
     for (var i = 0; i < cartLength; i++) {
 
       if (Object.keys(cart[i]) == item) {
-//        delete cart[i][item]
         cart.splice(i, 1)
         return cart
       }
